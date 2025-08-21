face92aa5ca971fef0dde556110d0378cc943b1a
Sumit Dev Nath <sumit.devnath1@gmail.com>
2025-08-21 19:17:30 +0600

    page contnet setup

--- app/news/page.jsx
+++ app/news/page.jsx
@@ -1,19 +1,25 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function NewsPage() {
   return (
     <>
       <h1>News Page</h1>
-      <ul>
-        <li>
-          <Link href="news/first-news">First News Items</Link>
-        </li>
-        <li>
-          <Link href="news/second-news">Second News Items</Link>
-        </li>
-        <li>
-          <Link href="news/third-news">Third News Items</Link>
-        </li>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newsItem) => (
+          <li key={newsItem.id}>
+            <Link href={`news/${newsItem.slug}`}>
+              <Image
+                src={`/images/news/${newsItem.image}`}
+                alt={newsItem.title}
+                width={100}
+                height={100}
+              />
+              <span>{newsItem.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
