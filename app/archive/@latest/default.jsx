61ec61a7e8888b4e2b2a2f6e68c98eb6e7f581b2
Sumit Dev Nath <sumit.devnath1@gmail.com>
2025-08-23 15:00:43 +0600

    parallel route with years and months

--- app/archive/@latest/default.jsx
+++ app/archive/@latest/default.jsx
@@ -5,7 +5,7 @@ export default function LatestNewsPage() {
   const latestNews = getLatestNews();
   return (
     <>
-      <h2>Latest News</h2>;
+      <h2>Latest News</h2>
       <NewsList news={latestNews} />
     </>
   );
