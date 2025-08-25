b7c824af0df0a1085ebe157b426dbb8b95e9bd7c
Sumit Dev Nath <sumit.devnath1@gmail.com>
2025-08-25 19:35:54 +0600

    local db and suspense for loading

---
+++ components/modal-backdrop.jsx
@@ -0,0 +1,7 @@
+import { useRouter } from "next/navigation";
+
+export default function ModalBackdrop() {
+  const router = useRouter();
+  <div className="modal-backdrop" onClick={router.back} />;
+  return;
+}
